f1c7b9620513a2fea220cfacec8c2528a4857373
Helena Anisic <helena.anisic@gmail.com>
2021-01-07 13:10:08 +0100

    nisam vise sigurna sta radim

---
+++ src/test/java/isa/apoteka/ApotekaApplicationTests.java
@@ -0,0 +1,13 @@
+package isa.apoteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApotekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
