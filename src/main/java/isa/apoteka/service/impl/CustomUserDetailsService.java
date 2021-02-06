153f00c6a4d89dc501e375ac21ea4c7da8341c61
Aleksandar Savic <aca24>
2021-02-06 15:09:50 +0100

    3.2 Password change fix

--- src/main/java/isa/apoteka/service/impl/CustomUserDetailsService.java
+++ src/main/java/isa/apoteka/service/impl/CustomUserDetailsService.java
@@ -44,27 +44,18 @@ public class CustomUserDetailsService implements UserDetailsService {
 	}
 
 	public void changePassword(String oldPassword, String newPassword) {
-
-		// Ocitavamo trenutno ulogovanog korisnika
-		Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
-		String username = currentUser.getName();
+		User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();;
+		String email = currentUser.getEmail();
 
 		if (authenticationManager != null) {
-			LOGGER.debug("Re-authenticating user '" + username + "' for password change request.");
-
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, oldPassword));
+			LOGGER.debug("Re-authenticating user '" + email + "' for password change request.");
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, oldPassword));
 		} else {
 			LOGGER.debug("No authentication manager set. can't change Password!");
-
 			return;
 		}
-
-		LOGGER.debug("Changing password for user '" + username + "'");
-
-		User user = (User) loadUserByUsername(username);
-
-		// pre nego sto u bazu upisemo novu lozinku, potrebno ju je hesirati
-		// ne zelimo da u bazi cuvamo lozinke u plain text formatu
+		LOGGER.debug("Changing password for user '" + email + "'");
+		User user = (User) loadUserByUsername(email);
 		user.setPassword(passwordEncoder.encode(newPassword));
 		userRepository.save(user);
 
