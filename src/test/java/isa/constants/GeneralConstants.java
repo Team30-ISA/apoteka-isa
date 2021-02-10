7bb5e0ed15e880f17de4bf9a1cc40e0cf6ca856a
Nikola Blesic <nikolablesic@gmail.com>
2021-02-10 18:44:59 +0100

    Add integration tests

---
+++ src/test/java/isa/constants/GeneralConstants.java
@@ -0,0 +1,10 @@
+package isa.constants;
+
+public class GeneralConstants {
+
+	public static final String LOGIN = "/auth/login";
+	public static final String DERM_EMAIL = "apoteka.isa2021+ale@gmail.com";
+	public static final String DERM_PASSWORD = "adminadmin";
+	public static final String PHARM_EMAIL = "apoteka.isa2021+jeca@gmail.com";
+	public static final String PHARM_PASSWORD = "adminadmin";
+}
